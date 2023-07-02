9c5455a5d1409888a468b10bf2b7c522c28d781d
yavuzim <kyavuzimer@gmail.com>
2023-07-02 11:44:26 +0300

    like, ilike

--- Homework3/query.sql
+++ Homework3/query.sql
@@ -7,5 +7,5 @@ where country like '%n' and length(country) > 5
 select title from film
 where title like '%t%t%t%t'
 
-select title, rental_rate,length from film
+select title, rental_rate,length from film 
 where title like 'C%' and length > 90 and rental_rate = 2.99
