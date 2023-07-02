a73ddf0b754cdd69f55875d838bd6a70c523d6a1
yavuzim <kyavuzimer@gmail.com>
2023-07-02 11:42:12 +0300

    select, where, mantiksal operatorler

--- Homework1/query.sql
+++ Homework1/query.sql
@@ -7,7 +7,7 @@ select * from film where length > 60 and length < 75
 select * from film
 where (rental_rate = 0.99) and (replacement_cost = 12.99 or replacement_cost = 28.99)
 
-select * from customer
+select * from customer 
 
 select last_name from customer where first_name = 'Mary'
 
